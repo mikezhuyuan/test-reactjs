fd68a8c440bbdb30675142ea1055ace5bedc6d39
mike.zhu <mikezhuyuan@hotmail.com>
2016-06-29 23:45:19 +1000

    add propTypes

--- src/TextField.jsx
+++ src/TextField.jsx
@@ -2,6 +2,15 @@ import * as React from 'react'
 import classNames from 'classnames'
 
 export class TextField extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.handleClick = this.handleClick.bind(this)
+        this.handleMouseEnter = this.handleMouseEnter.bind(this)
+        this.handleMouseLeave = this.handleMouseLeave.bind(this) 
+        this.handleBlur = this.handleBlur.bind(this)
+        this.handleFocus = this.handleFocus.bind(this)
+    }
     render() {
         let label = null
         let error = null
@@ -19,9 +28,9 @@ export class TextField extends React.Component {
         }
 
         return (
-            <div ref={ref => this._container = ref} onClick={this.handleClick.bind(this) } onMouseEnter={this.handleMouseEnter.bind(this)} onMouseLeave={this.handleMouseLeave.bind(this)} className={classNames('input-wrap', { required: this.props.required, error: Boolean(this.props.error) }) }>
+            <div ref={ref => this._container = ref} onClick={this.handleClick } onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave} className={classNames('input-wrap', { required: this.props.required, error: Boolean(this.props.error) }) }>
                 {label}
-                <input ref={ref => this._input = ref} onBlur={this.handleBlur.bind(this)} onFocus={this.handleFocus.bind(this)}  type="text" name="criteriaName" required={this.props.required} />
+                <input ref={ref => this._input = ref} onBlur={this.handleBlur} onFocus={this.handleFocus}  type="text" name="criteriaName" required={this.props.required} />
                 {error}
             </div>
         )
@@ -47,3 +56,8 @@ export class TextField extends React.Component {
         this._container.classList.add('input-wrap-focus')
     }
 }
+
+TextField.propTypes = {
+    label: React.PropTypes.string,
+    required: React.PropTypes.bool
+};
